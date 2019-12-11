b3d8cd3b93969e38a982e71ddc9dd81c8fd76715
HosseinJarrahi <hj021hj@gmail.com>
2019-12-11 20:43:44 +0330

    add username to user and chande db seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone','15');
-            $table->text('address');
+            $table->text('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
