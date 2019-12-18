393f7556aaf0469b21ad9993917151fd89f03a58
hossein jarrahi <hj021hj@gmail.com>
2019-12-18 22:07:17 +0330

    add aboutUs in manager but not working clean

--- routes/web.php
+++ routes/web.php
@@ -40,8 +40,7 @@ Route::post('reserve/{id?}' , 'HomeController@addReserve');
 
 // manager
 Route::group(['prefix' => 'manager','middleware' => 'auth'], function () {
-    Route::get("/", "ManagerController@show");
-    Route::get('home', "ManagerController@home")->name('admin.home');
+    Route::get("/", "ManagerController@show")->name('admin.home');
 //advertise
 
     Route::get('advertise', 'AdvertiseController@show');
