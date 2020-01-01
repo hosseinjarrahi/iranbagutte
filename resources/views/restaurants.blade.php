e39963b1d1dee7db077a62c9cda243268ce2cec6
hossein jarrahi <hj021hj@gmail.com>
2020-01-01 15:04:04 +0330

    restauraants page fixed

--- resources/views/restaurants.blade.php
+++ resources/views/restaurants.blade.php
@@ -8,18 +8,18 @@
     <div class="container-fluid mt-2">
         <div class="row p-2"></div>
         <div class="row mb-3 justify-content-center" style="overflow-x: hidden; direction: ltr !important">
-        @for($i=0;$i<30;$i++)
-                <a href="{{url('restaurants/1')}}" style="color: white">
+        @foreach($restaurants as $restaurant)
+                <a href="{{url('restaurant/'.$restaurant->id)}}" style="color: white">
                     <div class="res-ads ads-parent position-relative m-3 mt-2">
-                        <a href="#" style="color: #fff">
-                            <img class="img-fluid w-100 h-100" src="{{ asset('img/0219_Elmwood_0016.jpg') }}">
+{{--                        <a href="#" style="color: #fff">--}}
+                            <img class="img-fluid w-100 h-100" src="{{ asset('upload/'.$restaurant->pics[0]) }}">
                             <span class="IB-ads text-center w-100 p-2 position-absolute h-50">
-                               آدرس:هفت چنار میدان بریانک روبه روی مسجد المهدی
+                               {{ $restaurant->options->address ?? 'آدرسی ندارد' }}
                             </span>
-                        </a>
+{{--                        </a>--}}
                     </div>
                 </a>
-        @endfor
+        @endforeach
         </div>
 
 
