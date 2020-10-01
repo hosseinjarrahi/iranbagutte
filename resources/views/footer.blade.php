f948d843246b282b811f4f78be7fe5835e70064b
seyedalism <seyedalism@gmail.com>
2020-10-01 17:24:23 +0330

    change logo for applications

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,8 +4,8 @@
 <div class="container-fluid d-flex w-90 footer align-content-center align-items-center justify-content-center flex-column">
 	<p style="color: white;font-weight: bold;padding-top: 1em;">دانلود نسخه های موبایل ایران باگت</p>
 	<div>
-		<a href="{{($cyberspace[2]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="50px";height="50px" src="{{ asset('img/android.png') }}" alt="mac"></a>
-        <a href="{{($cyberspace[3]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="50px";height="50px" src="{{ asset('img/ios.png') }}" alt="android"></a>
+		<a href="{{($cyberspace[2]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="50px";height="50px" src="{{ asset('img/bazar.png') }}" alt="mac"></a>
+        <a href="{{($cyberspace[3]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="50px";height="50px" src="{{ asset('img/sibapp.png') }}" alt="android"></a>
         <a href="{{($cyberspace[4]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="160px";height="160px" src="{{ asset('img/myket.png') }}" alt="android"></a>
 	</div>
 
