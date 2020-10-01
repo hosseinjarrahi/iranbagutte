838cc0c059e67295d46549b93bf40089e30c0f2e
seyedalism <seyedalism@gmail.com>
2020-10-01 17:57:31 +0330

    change logo image

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,9 +4,9 @@
 <div class="container-fluid d-flex w-90 footer align-content-center align-items-center justify-content-center flex-column">
 	<p style="color: white;font-weight: bold;padding-top: 1em;">دانلود نسخه های موبایل ایران باگت</p>
 	<div>
-		<a href="{{($cyberspace[2]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="50px";height="50px" src="{{ asset('img/bazar.png') }}" alt="mac"></a>
-        <a href="{{($cyberspace[3]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="50px";height="50px" src="{{ asset('img/sibapp.png') }}" alt="android"></a>
-        <a href="{{($cyberspace[4]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="50px";height="50px" src="{{ asset('img/myket.png') }}" alt="android"></a>
+		<a href="{{($cyberspace[2]['url']) ?? '#'}}"><img style="padding-bottom: 1em;"  src="{{ asset('img/bazaar.png') }}" alt="mac"></a>
+        <a href="{{($cyberspace[3]['url']) ?? '#'}}"><img style="padding-bottom: 1em;"  src="{{ asset('img/sibapp.png') }}" alt="android"></a>
+        <a href="{{($cyberspace[4]['url']) ?? '#'}}"><img style="padding-bottom: 1em;"  src="{{ asset('img/myket.png') }}" alt="android"></a>
 	</div>
 
         <script  src="https://cdn.zarinpal.com/trustlogo/v1/trustlogo.js" type="text/javascript"></script>
