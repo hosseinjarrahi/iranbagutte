30f0510286d1ef5fbdaccec66a4478e4bfae0b6a
seyedalism <seyedalism@gmail.com>
2020-10-01 17:25:04 +0330

    some change in home

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -6,7 +6,7 @@
 	<div>
 		<a href="{{($cyberspace[2]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="50px";height="50px" src="{{ asset('img/bazar.png') }}" alt="mac"></a>
         <a href="{{($cyberspace[3]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="50px";height="50px" src="{{ asset('img/sibapp.png') }}" alt="android"></a>
-        <a href="{{($cyberspace[4]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="160px";height="160px" src="{{ asset('img/myket.png') }}" alt="android"></a>
+        <a href="{{($cyberspace[4]['url']) ?? '#'}}"><img style="padding-bottom: 1em;" width="50px";height="50px" src="{{ asset('img/myket.png') }}" alt="android"></a>
 	</div>
 
         <script  src="https://cdn.zarinpal.com/trustlogo/v1/trustlogo.js" type="text/javascript"></script>
